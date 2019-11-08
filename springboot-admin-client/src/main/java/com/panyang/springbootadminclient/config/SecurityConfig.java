d049e4d1f210b8b44f320e69801894d74e0ad82f
paddyang <paddyyang@hotmail.com>
2019-11-08 14:57:04 +0800

    增加springbootadmin

---
+++ springboot-admin-client/src/main/java/com/panyang/springbootadminclient/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.panyang.springbootadminclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and()
+                .authorizeRequests().antMatchers("/actuator/**").authenticated()
+                .anyRequest().permitAll();
+    }
+}
